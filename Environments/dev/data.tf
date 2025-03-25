ec845eaf2066954f8f7c8ace8b85dea9947dfe27
Nouman Nadeem <m.nouman.nadeem@gmail.com>
2025-03-25 16:06:58 +0500

    [UPDATE]: removed data resource

--- Environments/dev/data.tf
+++ Environments/dev/data.tf
@@ -5,19 +5,3 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
-}
